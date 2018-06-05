6b0a62dc50edca0b450ca9c6b9a7de8012d5654e
Sharon Mun <sharonymun@gmail.com>
2018-06-05 00:18:00 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,13 +7,13 @@ end
 "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(sleep)
-  while number > 0
-  puts "#{number} SECOND(S)!"
-  sleep(1)
-  number -= 1
-end
-"HAPPY NEW YEAR!"
+def countdown_with_sleep(number)
+  while (number > 0) do
+    puts "#{number} SECONDS!";
+    sleep(1);
+    number -= 1;
+  end
+  "HAPPY NEW YEAR!";
 end
 #def countdown_with_sleep(sleep)
   #sleep(5)
